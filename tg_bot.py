434e6b23cb7f1e2def9b52427a5c26401f1d01b2
levkozurev <lev453957@gmail.com>
2025-02-02 12:32:04 +0700

    Update tg_bot.py

--- tg_bot.py
+++ tg_bot.py
@@ -9,7 +9,20 @@ def main(message):
 
 @bot.message_handler(content_types=['photo'])
 def get_poto(message):
-        markup=types.InlineKeyboardMarkup()
-        markup.add(types.InlineKeyboardButton( 'Что мне сказать?',url='https://google.com' ) )
+        markup = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton( 'Как красиво!',url='https://google.com' )
+        markup.row(btn1)
+        btn2 = types.InlineKeyboardButton( 'удалить фото',callback_data='delete' )
+        btn3 = types.InlineKeyboardButton( 'изменить фото',callback_data='edit' )
+        markup.row(btn2,btn3)
         bot.reply_to(message, 'какое красивое фото', reply_markup = markup)
+
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+        if callback.data == 'delete':
+                bot.delete_message(callback.message.chat.id, callback.message.message_id-1)
+        elif callback.data == 'edit':
+                bot.edit_message_text('Edit text',callback.message.chat.id, callback.message.message_id)
+
+
 bot.polling(none_stop=True)
